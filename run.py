894e8a7a5a5ebc09cc47ffb726c15c00555a9e6f
s.dong <s.dong@mails.ccnu.edu.cn>
2020-08-07 09:44:22 +0800

    aviod repeating myself by using for...

--- run.py
+++ run.py
@@ -14,9 +14,8 @@ from lib.sca_device import ScaDevice
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-coloredlogs.install(level='DEBUG')
-coloredlogs.install(level='DEBUG', logger=log)
+log.setLevel(logging.INFO)
+coloredlogs.install(level='INFO', logger=log)
 
 __author__ = "Sheng Dong"
 __email__ = "s.dong@mails.ccnu.edu.cn"
@@ -31,37 +30,32 @@ if __name__ == '__main__':
     # global_dev.set_nuke()
     global_dev.set_soft_rst()
 
-
-	## Set SCA clocks
-    Fout_ref = 1000/120   ## Unit: MHz
-    Fout_dff = 1000/120  ## Unit: MHz
+    ## Set SCA clocks
+    Fout_ref = 1000 / 120  ## Unit: MHz
+    Fout_dff = 1000 / 120  ## Unit: MHz
     Precision = 0.1  ## Unit: Hundred percent, %
 
+    log.debug("MMCM initial Status:")
+    for chn in [0, 1]:
+        log.debug("clock {:} is locked: {:}!".format(chn, sca_dev.is_locked(chn)))
 
-    log.info("MMCM initial Status:")
-    chn = 0
-    log.info("clock {:} is locked: {:}!".format(chn, sca_dev.is_locked(chn)))
-    chn = 1
-    log.info("clock {:} is locked: {:}!".format(chn, sca_dev.is_locked(chn)))
-
-    sca_dev.set_frq(chn, Fout_ref, Precision)
-    time.sleep(1)
-    freq_ref = freq_ctr_dev.get_chn_freq(chn)
-    log.info("Tested Ref Clock frequency is : {}".format(freq_ref))
-
-    time.sleep(5)
-
-    chn = 1
-    sca_dev.set_frq(chn, Fout_dff, Precision)
-    time.sleep(1)
-    freq_dff = freq_ctr_dev.get_chn_freq(chn)
-    log.info("Tested Dff Clock frequency is : {}".format(freq_dff))
+    for chn in [0, 1]:
+        if chn == 0:
+            clock_name = "Ref"
+        else:
+            clock_name = "Dff"
+        sca_dev.set_frq(chn, Fout_ref, Precision)
+        time.sleep(1)
+        freq_ref = freq_ctr_dev.get_chn_freq(chn)
+        log.info("Tested {:s} Clock frequency is : {}".format(clock_name, freq_ref))
+        time.sleep(1)
 
     # Set Sca IO
     sca_dev.set_bit0(True)
     sca_dev.set_bit1(True)
-    sca_dev.start(True)
     sca_dev.dff_enable(True)
+    sca_dev.start(True)
+
     sca_dev.trigger(True)
     time.sleep(0.001)
     sca_dev.trigger(False)
